4a30852c938ce193039b33cc0a60908467206ae5
Matthew Walker <utoxin@gmail.com>
2021-08-25 16:58:15 -0600

    v1.0.2 released

--- train-stop-defaults/control.lua
+++ train-stop-defaults/control.lua
@@ -3,7 +3,10 @@ local event = require("__flib__.event")
 event.register(
         { defines.events.on_built_entity },
         function(event)
-            event.created_entity.trains_limit = settings.get_player_settings(event.player_index)["train-stop-defaults-user-default-train-limit-setting"]["value"]
+            local enabled = settings.get_player_settings(event.player_index)["train-stop-defaults-user-enable-limit-setting"]["value"]
+            if (enabled and (event.created_entity.trains_limit ~= nil and event.created_entity.trains_limit == 4294967295)) then
+                event.created_entity.trains_limit = settings.get_player_settings(event.player_index)["train-stop-defaults-user-default-train-limit-setting"]["value"]
+            end
         end,
         {
             { filter="type", type="train-stop" }
@@ -13,7 +16,10 @@ event.register(
 event.register(
         { defines.events.on_robot_built_entity },
         function(event)
-            event.created_entity.trains_limit = settings.global["train-stop-defaults-global-default-train-limit-setting"]["value"]
+            local enabled = settings.global["train-stop-defaults-global-enable-limit-setting"]["value"]
+            if (enabled and (event.created_entity.trains_limit ~= nil and event.created_entity.trains_limit == 4294967295)) then
+                event.created_entity.trains_limit = settings.global["train-stop-defaults-global-default-train-limit-setting"]["value"]
+            end
         end,
         {
             { filter="type", type="train-stop" }
--- train-stop-defaults/settings.lua
+++ train-stop-defaults/settings.lua
@@ -1,16 +1,32 @@
 data:extend({
     {
+        type = "bool-setting",
+        name = "train-stop-defaults-user-enable-limit-setting",
+        setting_type = "runtime-per-user",
+        default_value = true,
+        order = "0"
+    },
+    {
         type = "int-setting",
         name = "train-stop-defaults-user-default-train-limit-setting",
         setting_type = "runtime-per-user",
         default_value = 1,
-        minimum_value = 0
+        minimum_value = 0,
+        order = "1"
+    },
+    {
+        type = "bool-setting",
+        name = "train-stop-defaults-global-enable-limit-setting",
+        setting_type = "runtime-global",
+        default_value = true,
+        order = "0"
     },
     {
         type = "int-setting",
         name = "train-stop-defaults-global-default-train-limit-setting",
         setting_type = "runtime-global",
         default_value = 1,
-        minimum_value = 0
+        minimum_value = 0,
+        order = "1"
     }
 })
